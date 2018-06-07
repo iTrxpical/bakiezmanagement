ff9a792297003862838c04e01cfcd9ee7ee91afb
Askreno <39635631+Askreno@users.noreply.github.com>
2018-06-07 10:38:33 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,26 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ".";
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
-    client.user.setActivity('over people. | =info', { type: 'WATCHING' });
-    client.user.setStatus("online");
-});
-
-
-
-client.on('message', message => {
-
-    let sender = message.author;
-
-    if (sender.bot) return;
-    if (message.channel.type === 'dm') {
-        message.channel.send("**We can't read anything in DM's! Please go into a guild with me in and run the command `=info` for information about me!!!**")
-        return;
-    }
-    
-    var guildid = message.guild.id
-    
+   
     let msg = message.content.toLowerCase();
     let args = message.content.slice(prefix.length).trim().split(" ");
     let cmd = args.shift().toLowerCase();  
@@ -42,7 +23,7 @@ client.on('message', message => {
             console.log(`${message.author.username} ran the command: ${cmd} and is being passed onto the handler...!`);
 
         }
-})
+
     //IN COMMANDS FOLDER
     
     //MAKE A FILE NAMED THIS EXACTLY: 'ping.js' IT MUST BE CASE SENSITIVE AND HAVE .JS AT THE END. NO ''s.
